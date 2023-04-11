e6175857ca98c90397c21fe40c43f640fdcca3e7
Gabriel Martins Software Engineer <gabrielms.dev@gmail.com>
2023-04-11 20:10:03 -0300

    feat: changing bind

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<C-i>', builtin.diagnostics, {})
+vim.keymap.set('n', '<C-l>', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fp', function()
 	builtin.grep_string({search = vim.fn.input("Grep > ")})
 end)
