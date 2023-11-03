5dbc611e2213b009fc05b744616943e45e72e1c6
Gabriel Martins Software Engineer <gabrielms.dev@gmail.com>
2023-11-03 17:12:06 -0300

    feat: a lot of things

--- nvim/lua/custom/plugins/lazygit.lua
+++ nvim/lua/custom/plugins/lazygit.lua
@@ -1,6 +1,6 @@
 return {
-'kdheepak/lazygit.nvim',
-	config = function()
-		vim.keymap.set("n", "lg", ":LazyGit<CR>")
-	end
+    "kdheepak/lazygit.nvim",
+    config = function()
+        vim.keymap.set("n", "lg", ":LazyGit<CR>")
+    end
 }
---
+++ nvim/lua/custom/plugins/lint.lua
@@ -0,0 +1,20 @@
+return {
+    "mfussenegger/nvim-lint",
+    config = function()
+        local lint = require("lint")
+        lint.linters_by_ft = {
+            javascript = {"eslint"},
+            typescript = {"eslint"},
+            python = {"flake8"}
+        }
+
+        vim.api.nvim_create_autocmd(
+            "BufEnter",
+            {
+                callback = function()
+                    lint.try_lint()
+                end
+            }
+        )
+    end
+}
