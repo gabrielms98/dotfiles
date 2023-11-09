f7c8d2585bfd36addc8538765e5098313811a0bf
Gabriel Martins Software Engineer <gabrielms.dev@gmail.com>
2023-11-09 11:23:29 -0300

    feat: updating auto cmd

--- nvim/lua/custom/plugins/lint.lua
+++ nvim/lua/custom/plugins/lint.lua
@@ -5,11 +5,11 @@ return {
         lint.linters_by_ft = {
             javascript = {"eslint"},
             typescript = {"eslint"},
-            python = {"flake8"}
+            python = {"flake8"},
         }
 
         vim.api.nvim_create_autocmd(
-            "BufEnter",
+        {"BufEnter", "BufWritePost"},
             {
                 callback = function()
                     lint.try_lint()
