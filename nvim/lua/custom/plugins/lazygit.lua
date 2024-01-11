4e8d0af522df82d35440df8b5d98ee1228366b62
Gabriel Martins Software Engineer <gabrielms.dev@gmail.com>
2024-01-10 22:18:45 -0300

    refactor: change lazygit command

--- nvim/lua/custom/plugins/lazygit.lua
+++ nvim/lua/custom/plugins/lazygit.lua
@@ -1,6 +1,6 @@
 return {
     "kdheepak/lazygit.nvim",
     config = function()
-        vim.keymap.set("n", "lg", ":LazyGit<CR>")
+        vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
     end
 }
