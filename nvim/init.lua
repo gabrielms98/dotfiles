891cf4d06ca3a24ff905c85cc45892ce9063545d
Gabriel Martins Software Engineer <gabrielms.dev@gmail.com>
2025-05-14 13:50:29 -0300

    feat: aosrtieno

--- nvim/init.lua
+++ nvim/init.lua
@@ -381,76 +381,6 @@ require("lazy").setup(
                                     return client.supports_method(method, {bufnr = bufnr})
                                 end
                             end
-
-                            -- The following two autocommands are used to highlight references of the
-                            -- word under your cursor when your cursor rests there for a little while.
-                            --    See `:help CursorHold` for information about when this is executed
-                            --
-                            -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                            local client = vim.lsp.get_client_by_id(event.data.client_id)
-                            if
-                                client and
-                                    client_supports_method(
-                                        client,
-                                        vim.lsp.protocol.Methods.textDocument_documentHighlight,
-                                        event.buf
-                                    )
-                             then
-                                local highlight_augroup =
-                                    vim.api.nvim_create_augroup("kickstart-lsp-highlight", {clear = false})
-                                vim.api.nvim_create_autocmd(
-                                    {"CursorHold", "CursorHoldI"},
-                                    {
-                                        buffer = event.buf,
-                                        group = highlight_augroup,
-                                        callback = vim.lsp.buf.document_highlight
-                                    }
-                                )
-
-                                vim.api.nvim_create_autocmd(
-                                    {"CursorMoved", "CursorMovedI"},
-                                    {
-                                        buffer = event.buf,
-                                        group = highlight_augroup,
-                                        callback = vim.lsp.buf.clear_references
-                                    }
-                                )
-
-                                vim.api.nvim_create_autocmd(
-                                    "LspDetach",
-                                    {
-                                        group = vim.api.nvim_create_augroup("kickstart-lsp-detach", {clear = true}),
-                                        callback = function(event2)
-                                            vim.lsp.buf.clear_references()
-                                            vim.api.nvim_clear_autocmds {
-                                                group = "kickstart-lsp-highlight",
-                                                buffer = event2.buf
-                                            }
-                                        end
-                                    }
-                                )
-                            end
-
-                            -- The following code creates a keymap to toggle inlay hints in your
-                            -- code, if the language server you are using supports them
-                            --
-                            -- This may be unwanted, since they displace some of your code
-                            if
-                                client and
-                                    client_supports_method(
-                                        client,
-                                        vim.lsp.protocol.Methods.textDocument_inlayHint,
-                                        event.buf
-                                    )
-                             then
-                                map(
-                                    "<leader>th",
-                                    function()
-                                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {bufnr = event.buf})
-                                    end,
-                                    "[T]oggle Inlay [H]ints"
-                                )
-                            end
                         end
                     }
                 )
@@ -461,16 +391,6 @@ require("lazy").setup(
                     severity_sort = true,
                     float = {border = "rounded", source = "if_many"},
                     underline = {severity = vim.diagnostic.severity.ERROR},
-                    signs = vim.g.have_nerd_font and
-                        {
-                            text = {
-                                [vim.diagnostic.severity.ERROR] = "󰅚 ",
-                                [vim.diagnostic.severity.WARN] = "󰀪 ",
-                                [vim.diagnostic.severity.INFO] = "󰋽 ",
-                                [vim.diagnostic.severity.HINT] = "󰌶 "
-                            }
-                        } or
-                        {},
                     virtual_text = {
                         source = "if_many",
                         spacing = 2,
@@ -627,7 +547,7 @@ require("lazy").setup(
                 completion = {
                     -- By default, you may press `<c-space>` to show the documentation.
                     -- Optionally, set `auto_show = true` to show the documentation after a delay.
-                    documentation = {auto_show = true, auto_show_delay_ms = 500}
+                    documentation = {auto_show = true, auto_show_delay_ms = 200}
                 },
                 sources = {
                     default = {"lsp", "path", "snippets", "lazydev"},
