4913cb9f70cf3b8ee3a051a5588be0dea9032052
Gabriel Martins Software Engineer <gabrielms.dev@gmail.com>
2025-06-15 23:37:30 -0300

    feat: telescope additions

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,12 +2,25 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   dependencies = {
-    'folke/noice.nvim',
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      cond = function()
+        return vim.fn.executable "make" == 1
+      end
+    },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    -- Useful for getting pretty icons, but requires a Nerd Font.
+    { "nvim-tree/nvim-web-devicons",            enabled = vim.g.have_nerd_font }
   },
   config = function()
     require('telescope').setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown()
+        }
+      },
       defaults = {
         file_ignore_patterns = { 'node_modules', '.git' },
         mappings = {
@@ -17,17 +30,42 @@ return {
           },
         },
       },
-      find_files = { hidden = true }
+      find_files = {
+        hidden = true,
+      }
     })
 
-    require("telescope").load_extension("noice")
+    pcall(require("telescope").load_extension, "fzf")
+    pcall(require("telescope").load_extension, "ui-select")
 
     local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set(
       "n",
       "<C-p>",
-      function() builtin.find_files(require("telescope.themes").get_ivy({})) end,
+      function()
+        builtin.find_files(require("telescope.themes").get_ivy({}))
+      end,
       { desc = "[S]earch [F]iles" }
+    )
+
+
+    vim.keymap.set(
+      "n",
+      "<leader>fp",
+      function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      end
+    )
+    -- Shortcut for searching your Neovim configuration files
+    vim.keymap.set(
+      "n",
+      "<leader>sn",
+      function()
+        builtin.find_files { cwd = vim.fn.stdpath "config" }
+      end,
+      { desc = "[S]earch [N]eovim files" }
     )
   end
 }
