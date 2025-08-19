14ea56d94980d88b5e1c051abad83f27f7555307
Gabriel Martins <gabrielms.dev@gmail.com>
2025-08-19 16:06:30 -0300

    feat: fff layout

--- nvim/lua/plugins/fff.lua
+++ nvim/lua/plugins/fff.lua
@@ -2,7 +2,15 @@ return {
   "dmtrKovalenko/fff.nvim",
   build = "cargo build --release",
   opts = {
-    -- pass here all the options
+    keymaps = {
+      close = { '<C-c>', 'Esc' }
+    },
+    preview = {
+      line_numbers = true
+    },
+    layout = {
+      preview_position = "bottom"
+    }
   },
   keys = {
     {
