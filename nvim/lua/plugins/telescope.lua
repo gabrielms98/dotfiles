ede4faff2b0e853259bca2da5bcdc10156924ee9
Gabriel Martins Software Engineer <gabrielms.dev@gmail.com>
2025-12-17 21:45:58 -0300

    feat: diagnostics

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -41,7 +41,12 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    -- vim.keymap.set("n", "<C-p>", builtin.find_files)
+    vim.keymap.set("n", "<leader>l", function() builtin.diagnostics({ path_display = { "tail" }, bufnr = 0 }) end,
+      { desc = "Buffer diagnostics" }
+    )
+    vim.keymap.set("n", "<leader>L", function() builtin.diagnostics({ path_display = { "tail" } }) end,
+      { desc = "Workspace diagnostics" }
+    )
 
 
     vim.keymap.set(
